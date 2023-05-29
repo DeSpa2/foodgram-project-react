d496e00b148f617e341d093d62365a7d9cb6d97c
Денис Рассказов <DenSpas2@yandex.ru>
2023-05-30 00:45:48 +0700

    deploy

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -29,8 +29,8 @@ class Tag(models.Model):
 
     class Meta:
         ordering = ('-id',)
-        verbose_name = 'Теg'
-        verbose_name_plural = 'Теgи'
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
 
     def __str__(self):
         return self.name
