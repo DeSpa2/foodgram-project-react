f5524aaa9760c1c9bf87d5d5fa44ed7c5cf4ac60
Денис Рассказов <DenSpas2@yandex.ru>
2023-05-30 23:57:31 +0700

    deploy

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -111,12 +111,17 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 6,
+    'DEFAULT_FILTER_BACKENDS': 'rest_framework.filters.SearchFilter',
+    'SEARCH_PARAM': 'name',
 }
 
 DJOSER = {
