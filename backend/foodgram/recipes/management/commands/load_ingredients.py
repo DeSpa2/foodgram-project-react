52aed228dade2a129cfff18983d460649e7ee1bd
Денис Рассказов <DenSpas2@yandex.ru>
2023-06-02 00:32:00 +0700

    deploy

--- backend/foodgram/recipes/management/commands/load_ingredients.py
+++ backend/foodgram/recipes/management/commands/load_ingredients.py
@@ -1,23 +1,35 @@
-import csv
+import json
 
 from django.core.management.base import BaseCommand
 
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag
 
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **options):
-        self.import_ingredients()
-        print('Ингредиенты загружены.')
+    def add_arguments(self, parser):
+        parser.add_argument("--path", type=str, help="file path")
 
-    def import_ingredients(self, file='ingredients.csv'):
-        print(f'Загрузка {file}...')
-        file_path = f'./data/{file}'
-        with open(file_path, newline='', encoding='utf-8') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                status, created = Ingredient.objects.update_or_create(
-                    name=row[0],
-                    measurement_unit=row[1]
-                )
+    def handle(self, *args, **options):
+        file_path = options["path"]
+
+        with open(file_path, encoding='utf-8') as f:
+            jsondata = json.load(f)
+            if 'color' in jsondata[0]:
+                for line in jsondata:
+                    if not Tag.objects.filter(
+                       slug=line['slug']).exists():
+                        Tag.objects.create(
+                            name=line['name'],
+                            color=line['color'],
+                            slug=line['slug'],
+                        )
+            elif 'measurement_unit' in jsondata[0]:
+                for line in jsondata:
+                    if not Ingredient.objects.filter(
+                       name=line['name'],
+                       measurement_unit=line['measurement_unit']).exists():
+                        Ingredient.objects.create(
+                            name=line['name'],
+                            measurement_unit=line['measurement_unit']
+                        )
