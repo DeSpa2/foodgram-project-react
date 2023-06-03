2ae26a713eb618886a344a4875136275a6cbd724
Денис Рассказов <DenSpas2@yandex.ru>
2023-06-03 23:40:49 +0700

    deploy

--- backend/foodgram/recipes/management/commands/parse_ingredients_csv.py
+++ backend/foodgram/recipes/management/commands/parse_ingredients_csv.py
@@ -8,20 +8,20 @@ PATH = "foodgram/data"
 
 
 class Command(BaseCommand):
-    help = "import data from ingredients.csv"
+    help = "Импорт из ingredients.csv"
 
     def handle(self, *args, **options):
         with open(f'{PATH}/ingredients.csv', 'r') as file:
             reader = csv.reader(file)
             for row in reader:
                 name = row[0]
-                # Здесь используется метод get_or_create() для избежания дубликатов
-                ingredient, created = Ingredient.objects.get_or_create(name=name)
+                measurement_unit = row[1]
+                ingredient, created = Ingredient.objects.get_or_create(name=name, measurement_unit=measurement_unit)
                 if created:
                     self.stdout.write(
-                        self.style.SUCCESS(f'Successfully added ingredient "{name}"')
+                        self.style.SUCCESS(f'Ингредиент добавлен "{name}" с единицей измерения "{measurement_unit}"')
                     )
                 else:
                     self.stdout.write(
-                        self.style.WARNING(f'Ingredient "{name}" already exists')
+                        self.style.WARNING(f'Ингредиент "{name}" с единицей измерения "{measurement_unit}" не добавлен')
                     )
