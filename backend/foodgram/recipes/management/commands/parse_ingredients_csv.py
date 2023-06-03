93462058a70fb3204111bf0c13daee1f4a6f25dd
Денис Рассказов <DenSpas2@yandex.ru>
2023-06-04 01:00:53 +0700

    bug fix

--- backend/foodgram/recipes/management/commands/parse_ingredients_csv.py
+++ backend/foodgram/recipes/management/commands/parse_ingredients_csv.py
@@ -16,13 +16,13 @@ class Command(BaseCommand):
             for row in reader:
                 name = row[0]
                 measurement_unit = row[1]
+                if name == 'пекарский порошок':
+                    continue
                 ingredient, created = Ingredient.objects.get_or_create(name=name, measurement_unit=measurement_unit)
                 if created:
                     self.stdout.write(
                         self.style.SUCCESS(f'Ингредиент добавлен "{name}" с единицей измерения "{measurement_unit}"')
                     )
-                elif name == 'пекарский порошок':
-                    continue
                 else:
                     self.stdout.write(
                         self.style.WARNING(f'Ингредиент "{name}" с единицей измерения "{measurement_unit}" не добавлен')
