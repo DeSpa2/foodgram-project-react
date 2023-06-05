bb7bd2247c5500d307d72dac70a58a5b008b1496
Денис Рассказов <DenSpas2@yandex.ru>
2023-06-05 17:32:36 +0700

    deploy

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -20,7 +20,7 @@ class CustomUserViewSet(UserViewSet):
 
     @action(detail=False, permission_classes=(IsAuthenticatedOrAdmin,))
     def subscriptions(self, request):
-        follows = User.objects.filter(following__author=request.author)
+        follows = Follow.objects.filter(following=self.get_object())
         pages = self.paginate_queryset(follows)
         serializer = FollowSerializer(
             pages,
